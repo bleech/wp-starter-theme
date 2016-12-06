de919b67c5dc65776c8274fce27ddd24ab8fc206
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-12-06 18:29:08 +0100

    feat(PostList): Removed Linter Errors in Blog Filter

--- lib/DataFilters/Blog.php
+++ lib/DataFilters/Blog.php
@@ -5,7 +5,10 @@ namespace WPStarterTheme\DataFilters;
 use WPStarterTheme\Helpers\StringHelpers;
 use WPStarterTheme\Helpers\Log;
 
-add_filter('WPStarterTheme/DataFilters/PostButtonNavigation', ['WPStarterTheme\DataFilters\Blog', 'setPostButtonNavigationData'], 10, 3);
+add_filter('WPStarterTheme/DataFilters/PostButtonNavigation', [
+  'WPStarterTheme\DataFilters\Blog',
+  'setPostButtonNavigationData'
+], 10, 3);
 
 class Blog {
 
