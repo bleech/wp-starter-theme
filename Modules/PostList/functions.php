9a64a4e0995b9f2e9856e9e77351d5911c33d22d
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-12-06 15:59:50 +0100

    feat(PostList): Added real data from the database to the PostList Module

--- Modules/PostList/functions.php
+++ Modules/PostList/functions.php
@@ -10,140 +10,44 @@ add_image_size('wpsPostListSm', 768, 768, true);
 
 add_filter('WPStarter/modifyModuleData?name=PostList', function ($data) {
   $data['readMoreTitle'] = 'Read more';
-  
+
   $data['posts'] = apply_filters('WPStarterTheme/DataFilters/Posts', [], 5, 'long');
-  $data['posts'] = [
-    [
-      'title' => '1 Hello World, this is a demo post',
-      'url' => '#',
-      'image' => 'http://placehold.it/768x768',
-      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      'tags' => [
-        [
-          'name' => 'News',
-          'url' => '#'
-        ],
-        [
-          'name' => 'Info',
-          'url' => '#'
-        ]
-      ],
-      'categories' => [
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ],
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ]
+  $data['posts'] = array_map(function($post) {
+    $tags = get_the_tags($post->ID);
+    if($tags) {
+      $tags = array_map(function($tag) {
+        return [
+          'name' => $tag->name,
+          'url' => get_tag_link($tag->term_id)
+        ];
+      }, $tags);
+    }
+    $categories = get_the_category($post->ID);
+    if($categories) {
+      $categories = array_map(function($cat) {
+        return [
+          'name' => $cat->name,
+          'url' => get_category_link($cat->term_id)
+        ];
+      }, $categories);
+    }
+    $postImage = get_field('thumbnail', $post->ID);
+    $postImage['imageConfig'] = [
+      'default' => 'wpsPostListLg',
+      'sizes' => [
+        'wpsPostListSm' => '(max-width: 767px)'
       ]
-    ],
-    [
-      'title' => '2 Hello World, this is a demo post',
-      'url' => '#',
-      'image' => 'http://placehold.it/768x768',
-      'content' => 'This is a short excerpt for this post we should use have in here',
-      'tags' => [
-        [
-          'name' => 'News',
-          'url' => '#'
-        ],
-        [
-          'name' => 'Info',
-          'url' => '#'
-        ]
-      ],
-      'categories' => [
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ],
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ]
-      ]
-    ],
-    [
-      'title' => '3 Hello World, this is a demo post',
-      'url' => '#',
-      'image' => 'http://placehold.it/768x768',
-      'content' => 'This is a short excerpt for this post we should use have in here',
-      'tags' => [
-        [
-          'name' => 'News',
-          'url' => '#'
-        ],
-        [
-          'name' => 'Info',
-          'url' => '#'
-        ]
-      ],
-      'categories' => [
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ],
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ]
-      ]
-    ],
-    [
-      'title' => '4 Hello World, this is a demo post',
-      'url' => '#',
-      'image' => 'http://placehold.it/768x768',
-      'content' => 'This is a short excerpt for this post we should use have in here',
-      'tags' => [
-        [
-          'name' => 'News',
-          'url' => '#'
-        ],
-        [
-          'name' => 'Info',
-          'url' => '#'
-        ]
-      ],
-      'categories' => [
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ],
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ]
-      ]
-    ],
-    [
-      'title' => 'Hello World, this is a demo post',
-      'url' => '#',
-      'image' => 'http://placehold.it/768x768',
-      'content' => 'This is a short excerpt for this post we should use have in here',
-      'tags' => [
-        [
-          'name' => 'News',
-          'url' => '#'
-        ],
-        [
-          'name' => 'Info',
-          'url' => '#'
-        ]
-      ],
-      'categories' => [
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ],
-        [
-          'name' => 'A Category',
-          'url' => '#'
-        ]
-      ]
-    ]
-  ];
+    ];
+
+    return [
+      'title' => $post->title,
+      'content' => get_field('excerpt', $post->ID),
+      'image' => $postImage,
+      'url' => $post->url,
+      'tags' => $tags,
+      'categories' => $categories
+    ];
+  }, $data['posts']);
 
   return $data;
 });
