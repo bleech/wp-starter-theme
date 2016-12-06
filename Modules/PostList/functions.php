d5aadbbeb8108d614ed88e3f4f95fb8dd22e4b27
Dominik Biedebach <dominik.biedebach@gmail.com>
2016-12-06 19:15:07 +0100

    feat(Linter): Removed Linting Errors

--- Modules/PostList/functions.php
+++ Modules/PostList/functions.php
@@ -11,21 +11,23 @@ add_image_size('wpsPostListSm', 768, 768, true);
 add_filter('WPStarter/modifyModuleData?name=PostList', function ($data, $parentData) {
   $data['readMoreTitle'] = 'Read more';
 
-  $data['posts'] = array_map(function($post) {
+  $data['posts'] = array_map(function ($post) {
     $tags = get_the_tags($post['ID']);
-    if($tags) {
-      $tags = array_map(function($tag) {
+    if ($tags) {
+      $tags = array_map(function ($tag) {
         return [
           'name' => $tag->name,
+          // @codingStandardsIgnoreLine
           'url' => get_tag_link($tag->term_id)
         ];
       }, $tags);
     }
     $categories = get_the_category($post['ID']);
-    if($categories) {
-      $categories = array_map(function($cat) {
+    if ($categories) {
+      $categories = array_map(function ($cat) {
         return [
           'name' => $cat->name,
+          // @codingStandardsIgnoreLine
           'url' => get_category_link($cat->term_id)
         ];
       }, $categories);
