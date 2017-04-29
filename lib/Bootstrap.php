967fab3a06bae13601851f218354a89bb8b5c466
Doğa Gürdal <doga@bleech.de>
2017-04-29 11:34:38 +0200

    style(lib/Bootstrap): remove whitespace, remove unused use statement

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -2,14 +2,10 @@
 
 namespace Flynt;
 
-use RecursiveDirectoryIterator;
-
 class Bootstrap
 {
-
     public static function setTemplateDirectory()
     {
-
         add_action('after_switch_theme', function () {
             $stylesheet = get_option('stylesheet');
 
@@ -25,14 +21,13 @@ class Bootstrap
 
     public static function checkPlugin()
     {
-
         $pluginActive = class_exists('\\Flynt\\Render');
 
         if (!$pluginActive) {
             add_action('admin_notices', function () {
                 echo '<div class="error"><p>Flynt Core Plugin not activated. Make sure you activate the plugin in <a href="'
-                . esc_url(admin_url('plugins.php#flynt')) . '">'
-                . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+                    . esc_url(admin_url('plugins.php#flynt')) . '">'
+                    . esc_url(admin_url('plugins.php')) . '</a></p></div>';
             });
 
             add_filter('template_include', function () {
@@ -41,8 +36,8 @@ class Bootstrap
                     return $newTemplate;
                 } else {
                     return 'Flynt Core Plugin not activated! Please <a href="'
-                    . esc_url(admin_url('plugins.php'))
-                    . '">activate the plugin</a> and reload the page.';
+                        . esc_url(admin_url('plugins.php'))
+                        . '">activate the plugin</a> and reload the page.';
                 }
             });
         }
