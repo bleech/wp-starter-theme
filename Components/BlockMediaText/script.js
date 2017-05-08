ea230b09d23349bd18b25f451d9e6ee2054aa9d2
Markus Müller <markus.mueller@live.de>
2017-05-08 20:15:45 +0200

    feat(Components/BlockMediaText): refactored component

---
+++ Components/BlockMediaText/script.js
@@ -0,0 +1,40 @@
+class BlockMediaText extends window.HTMLDivElement {
+  constructor (self) {
+    self = super(self)
+    self.$ = $(self)
+    self.resolveElements()
+    return self
+  }
+
+  resolveElements () {
+    this.resolveOembedVideoElements()
+  }
+
+  connectedCallback () {
+    this.connectedOembedVideoCallback()
+  }
+
+  resolveOembedVideoElements () {
+    this.$posterImage = $('.oembedVideo-posterImage', this)
+    this.$video = $('.oembedVideo-video', this)
+    this.$iframe = $('iframe', this)
+  }
+
+  connectedOembedVideoCallback () {
+    this.$iframe.on('load', this.onIframeLoad.bind(this))
+    this.$posterImage.on('click', (e) => {
+      this.setIframeSrc()
+    })
+  }
+
+  setIframeSrc = () => {
+    this.$iframe.attr('src', this.$iframe.data('src'))
+  }
+
+  onIframeLoad () {
+    this.$video.addClass('oembedVideo-video-isVisible')
+    this.$posterImage.addClass('oembedVideo-posterImage-isHidden')
+  }
+}
+
+window.customElements.define('flynt-block-media-text', BlockMediaText, {extends: 'div'})
