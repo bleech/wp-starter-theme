c696cd2831158091d6b4eaa7903e04e25a1fbe2b
Markus Müller <markus.mueller@live.de>
2017-05-11 15:48:24 +0200

    style(Features/TimberHelper): fixed coding style

--- lib/Utils/TimberHelper.php
+++ lib/Utils/TimberHelper.php
@@ -4,30 +4,32 @@ namespace Flynt\Utils;
 
 use Timber\Post;
 
-class TimberHelper {
-  public static function getTimberPostById($postId, $moreLinkText = null, $stripteaser = false) {
-    global $post;
-    $post = get_post($postId);
-    setup_postdata($post, $moreLinkText, $stripteaser);
-    $timberPost = new Post($post);
-    wp_reset_postdata($post);
-    return $timberPost;
-  }
+class TimberHelper
+{
+    public static function getTimberPostById($postId, $moreLinkText = null, $stripteaser = false)
+    {
+        global $post;
+        $post = get_post($postId);
+        setup_postdata($post, $moreLinkText, $stripteaser);
+        $timberPost = new Post($post);
+        wp_reset_postdata($post);
+        return $timberPost;
+    }
 
-  public static function getExcerpt(
-    $postId,
-    $len = 50,
-    $force = false,
-    $readmore = "Read More",
-    $strip = true,
-    $end = "&hellip;"
-  ) {
-    global $post;
-    $post = get_post($postId);
-    setup_postdata($post, null, false);
-    $timberPost = new Post($post);
-    $excerpt = $timberPost->get_preview($len, $force, $readmore, $strip, $end);
-    wp_reset_postdata($post);
-    return $excerpt;
-  }
+    public static function getExcerpt(
+        $postId,
+        $len = 50,
+        $force = false,
+        $readmore = "Read More",
+        $strip = true,
+        $end = "&hellip;"
+    ) {
+        global $post;
+        $post = get_post($postId);
+        setup_postdata($post, null, false);
+        $timberPost = new Post($post);
+        $excerpt = $timberPost->get_preview($len, $force, $readmore, $strip, $end);
+        wp_reset_postdata($post);
+        return $excerpt;
+    }
 }
