eace03b0383d8bdc4d18f531ccd59aa7b7138e1c
Wahiba Khadri <wahiba@bleech.de>
2017-06-01 10:40:01 +0200

    fix(Log class): Pretty print fix and code refactoring
    Pretty print filename fix. String interpolation. Simple quotes instead of double. Unnecessary data
    assignment removed.

--- lib/Utils/Log.php
+++ lib/Utils/Log.php
@@ -6,17 +6,17 @@ class Log
 {
     public static function console($data, $postpone = true)
     {
-        $data = self::consoleDebug($data, $postpone);
+        self::consoleDebug($data, $postpone);
     }
 
     public static function error($data, $postpone = true)
     {
-        $data = self::consoleDebug($data, $postpone, 'PHP', 'error');
+        self::consoleDebug($data, $postpone, 'PHP', 'error');
     }
 
     public static function pp($data, $postpone = true)
     {
-        $data = self::printDebug($data, $postpone);
+        self::printDebug($data, $postpone);
     }
 
     public static function consoleDebug($data, $postpone, $title = 'PHP', $logType = 'log')
@@ -25,9 +25,9 @@ class Log
         $type = gettype($data);
         if (is_array($data) || is_object($data)) {
             $output = json_encode($data);
-            $result =  "<script>console.$logType('$title', '($type)', $output);</script>\n";
+            $result =  "<script>console.{$logType}('{$title}', '({$type})', {$output});</script>\n";
         } else {
-            $result = "<script>console.$logType('$title', '($type)', '$data');</script>\n";
+            $result = "<script>console.{$logType}('{$title}', '({$type})', '{$data}');</script>\n";
         }
         self::echoDebug($result, $postpone);
     }
@@ -35,12 +35,12 @@ class Log
     public static function printDebug($data, $postpone)
     {
         $type = gettype($data);
-        $output = "<pre>";
-        $output .= "(" . $type . ") ";
+        $output = '<pre>';
+        $output .= '(' . $type . ') ';
         ob_start();
         print_r($data);
         $output .= ob_get_clean();
-        $output .= "<br />File: <strong>" . self::getCallerFile() . "</strong>";
+        $output .= '<br />File: <strong>' . self::getCallerFile(2) . '</strong>';
         $output .= "</pre>\n";
         self::echoDebug($output, $postpone);
     }
