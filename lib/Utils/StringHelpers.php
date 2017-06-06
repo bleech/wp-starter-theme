29a22cbc8b3d9252b964372e04caeb3764e59dd5
Wahiba Khadri <wahiba@bleech.de>
2017-06-06 17:58:12 +0200

    feat(lib/Utils/StringHelpers.php): Added DocBlocks

--- lib/Utils/StringHelpers.php
+++ lib/Utils/StringHelpers.php
@@ -4,11 +4,27 @@ namespace Flynt\Utils;
 
 class StringHelpers
 {
+    /**
+    * Converts a string from Camel case to Kebap case.
+    *
+    * @since 0.1.0
+    *
+    * @param string $str  The string to convert.
+    */
     public static function camelCaseToKebap($str)
     {
         return strtolower(preg_replace('/([a-zA-Z])(?=[A-Z])/', '$1-', $str));
     }
 
+    /**
+    * Strip all HTML tags including script and style,
+    * and trims text to a certain number of words.
+    *
+    * @since 0.1.0
+    *
+    * @param string $str    The string to trim and strip.
+    * @param number $length The string lenght to return.
+    */
     public static function trimStrip($str, $length = 25)
     {
         if (isset($str)) {
@@ -17,6 +33,13 @@ class StringHelpers
         return $str;
     }
 
+    /**
+    * Splits a Camel case string.
+    *
+    * @since 0.1.0
+    *
+    * @param string $str  The string to split.
+    */
     public static function splitCamelCase($str)
     {
         $a = preg_split(
@@ -29,6 +52,14 @@ class StringHelpers
         return implode($a, ' ');
     }
 
+    /**
+    * Converts a string from Kebap case to Camel case.
+    *
+    * @since 0.1.0
+    *
+    * @param string $str                        The string to convert.
+    * @param boolean $capitalizeFirstCharacter  Sets if the first character should be Capitalized.
+    */
     public static function kebapCaseToCamelCase($str, $capitalizeFirstCharacter = false)
     {
         $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $str)));
@@ -38,6 +69,14 @@ class StringHelpers
         return $str;
     }
 
+    /**
+    * Removes a prefix from a string.
+    *
+    * @since 0.1.0
+    *
+    * @param string $prefix The prefix to be removed.
+    * @param string $str    The entire string to manipulate.
+    */
     public static function removePrefix($prefix, $str)
     {
         if (substr($str, 0, strlen($prefix)) == $prefix) {
@@ -46,11 +85,31 @@ class StringHelpers
         return $str;
     }
 
+    /**
+    * Checks if a string starts with a certain string.
+    *
+    * @since 0.1.0
+    *
+    * @param string $search   The string to search.
+    * @param string $subject  The string to look into.
+
+    * @return boolean Returns true if the subject string starts with the search string.
+    */
     public static function startsWith($search, $subject)
     {
         return substr($subject, 0, strlen($search)) === $search;
     }
 
+    /**
+    * Checks if a string ends with a certain string.
+    *
+    * @since 0.1.0
+    *
+    * @param string $search   The string to search.
+    * @param string $subject  The string to look into.
+
+    * @return boolean Retunrs true if the subject string ends with the search string.
+    */
     public static function endsWith($search, $subject)
     {
         $searchLength = strlen($search);
