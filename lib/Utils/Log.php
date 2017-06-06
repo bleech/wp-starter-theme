ade995b0c45b116b345e6e5cfd051761f0938456
Wahiba Khadri <wahiba@bleech.de>
2017-06-06 16:20:59 +0200

    feat(lib/Utils/Log.php): Added DocBlocks

--- lib/Utils/Log.php
+++ lib/Utils/Log.php
@@ -4,21 +4,65 @@ namespace Flynt\Utils;
 
 class Log
 {
+    /**
+    * Outputs a message to the Web Console.
+    *
+    * Outputs a JavaScript console.log
+    *
+    * @since 0.1.0
+    *
+    * @param mixed    $data     data to be logged.
+    * @param boolean  $postpone postpone logging to the wp_footer action.
+    */
     public static function console($data, $postpone = true)
     {
         self::consoleDebug($data, $postpone);
     }
 
+    /**
+    * Outputs an error message to the Web Console.
+    *
+    * Outputs a JavaScript error.log
+    *
+    * @since 0.1.0
+    *
+    * @param mixed    $data     data to be logged.
+    * @param boolean  $postpone postpone logging to the wp_footer action.
+    */
     public static function error($data, $postpone = true)
     {
         self::consoleDebug($data, $postpone, 'PHP', 'error');
     }
 
+    /**
+    * Pretty prints a variable.
+    *
+    * Prints human-readable information about a variable.
+    *
+    * @since 0.1.0
+    *
+    * @param mixed    $data  data to be printed.
+    * @param boolean  $postpone postpone printing to the wp_footer action.
+    */
     public static function pp($data, $postpone = true)
     {
         self::printDebug($data, $postpone);
     }
 
+    /**
+    * Outputs a message to the Web Console.
+    *
+    * Outputs a JavaScript log.
+    * Accepts log, error and trace. Can specify a title.
+    * Will also output the data type, the filename and line number.
+    *
+    * @since 0.1.0
+    *
+    * @param mixed    $data     data to be logged.
+    * @param boolean  $postpone postpone logging to the wp_footer action.
+    * @param string   $title     title.
+    * @param string   $logType   type of log (accepts log|error|trace).
+    */
     public static function consoleDebug($data, $postpone = true, $title = 'PHP', $logType = 'log')
     {
         if (in_array($logType, ['log', 'error', 'trace'])) {
@@ -30,6 +74,18 @@ class Log
         }
     }
 
+    /**
+    * Pretty prints a variable.
+    *
+    * Prints human-readable information about a variable.
+    * Print will be wrapped between <pre> tags.
+    * Will also output the data type, the filename and line number.
+    *
+    * @since 0.2.0
+    *
+    * @param mixed    $data  data to be printed.
+    * @param boolean  $postpone postpone printing to the wp_footer action.
+    */
     public static function printDebug($data, $postpone = true)
     {
         $type = gettype($data);
@@ -43,6 +99,14 @@ class Log
         self::echoDebug($output, $postpone);
     }
 
+    /**
+    * Echoes a variable.
+    *
+    * @since 0.2.0
+    *
+    * @param mixed    $data     data to be printed.
+    * @param boolean  $postpone postpone printing to the wp_footer action.
+    */
     protected static function echoDebug($data, $postpone)
     {
         if ($postpone) {
@@ -54,6 +118,13 @@ class Log
         }
     }
 
+    /**
+    * Returns a filename and line number by performing a debug_backtrace.
+    *
+    * @since 0.1.0
+    *
+    * @param number $depth  depth to return.
+    */
     protected static function getCallerFile($depth = 1)
     {
         $debug = debug_backtrace();
